7b5445366062b9b1dee3a1419e0c74585f4c867d
Daddy The Runner <DaddyTheRunner@users.noreply.github.com>
2015-01-11 16:56:39 -0500

    Added time series plot and analysis of the daily activity averages.

--- testing.R
+++ testing.R
@@ -39,3 +39,32 @@ hist.plot <- ggplot(daily.data, aes(x=steps)) +
 
 ## Display the plot
 hist.plot
+
+
+## Calculate the hours and minutes for each interval
+data <- data %>% 
+  mutate(hour = interval %/% 100,
+         min = interval %% 100,
+         timestr = sprintf("%02i:%02i", hour, min),
+         time = as.POSIXct(timestr, format="%H:%M"))
+
+## generate the time series data
+ts.data <- data %>%
+  group_by(time) %>%
+  summarize(avg.steps = mean(steps, na.rm = TRUE))
+
+## Generate the time series plot
+ts.plot <- ggplot(ts.data, aes(x=time, y=avg.steps)) +
+  geom_line() +
+  scale_x_datetime(labels = date_format("%H:%M"),
+                   breaks = "4 hour", minor_breaks = "1 hour") +
+  xlab("Time of Day") +
+  ylab("Steps") +
+  ggtitle("Average Steps Taken\n(5 minute intervals)\n") +
+  theme(plot.title = element_text(lineheight=.8, face="bold"))
+
+# Find the max average number of steps
+max(ts.data$avg.steps)
+
+## The following expression gives the time of the max
+format(ts.data$time[which.max(ts.data$avg.steps)], format="%H:%M")
